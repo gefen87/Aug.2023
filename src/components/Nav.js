1d3006599e00a721d3b51718b818982c541dc58e
gefen87 <120755249+gefen87@users.noreply.github.com>
2023-07-27 00:09:53 +0300

    booking-1

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,20 +1,21 @@
-import React  from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from './lemon-logo.png';
-const Nav = () => {
 
-    return (
-        <nav className="main-nav">
-        <ul>
-            <li><img src={logo} alt="logo"/></li>
-            <li><a href="url">Home</a></li>
-            <li><a href="url">About</a></li>
-            <li><a href="url">Menu</a></li>
-            <li><a href="url">Reservations</a></li>
-            <li><a href="url">Order Online</a></li>
-            <li><a href="url">Login</a></li>
-        </ul>
-        </nav>
-    );
+const Nav = () => {
+  return (
+    <nav className="main-nav">
+      <ul>
+        <li><img src={logo} alt="logo" /></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/menu">Menu</Link></li>
+        <li><Link to="./components/BookingPage">Reservations</Link></li>
+        <li><Link to="/order-online">Order Online</Link></li>
+        <li><Link to="/login">Login</Link></li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Nav;
