5edc879ba4c33a6564ff2b9c7cc1666a2b383c00
gefen87 <120755249+gefen87@users.noreply.github.com>
2023-07-26 11:53:26 +0300

    no router yes yarn

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Head from './components/Head';
 import Nav from './components/Nav.js';
@@ -11,41 +10,39 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <div>
-          <Head />
-        </div>
-        <div>
-          <Nav />
-        </div>
-        <div>
-          <Route exact path="/" component={Main} />
-        </div>
-        <div className='aside'>
-          <h1>This Weeks Specials!</h1>
-          <button>Online Menu</button>
-          <div className='part'
-            style={{
-              display: "flex",
-              width: "200px",
-              height: "100px",
-              margin: "12rem",
-              gap: "5rem",
-              position: "relative",
-              left: "50%"
-            }}
-          >
-            <Aside1 />
-            <Aside2 />
-            <Aside3 />
-          </div>
-        </div>
-        <div>
-          <Footer />
+    <div className="App">
+      <div>
+        <Head />
+      </div>
+      <div>
+        <Nav />
+      </div>
+      <div>
+        <Main />
+      </div>
+      <div className='aside'>
+        <h1>This Weeks Specials!</h1>
+        <button>Online Menu</button>
+        <div className='part'
+          style={{
+            display: "flex",
+            width: "200px",
+            height: "100px",
+            margin: "12rem",
+            gap: "5rem",
+            position: "relative",
+            left: "50%"
+          }}
+        >
+          <Aside1 />
+          <Aside2 />
+          <Aside3 />
         </div>
       </div>
-    </Router>
+      <div>
+        <Footer />
+      </div>
+    </div>
   );
 }
 
