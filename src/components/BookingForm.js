dccfe374e90635e5fef37cdb95dcfee20db78730
gefen87 <120755249+gefen87@users.noreply.github.com>
2023-07-29 01:22:47 +0300

    28.7.2023

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,7 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import './BookingForm.css';
 
 const BookingForm = ({ availableTimes, submitForm }) => {
   const initialFormState = {
+    name: '',
+    phoneNumber: '',
+    email: '',
     date: '',
     time: availableTimes[0],
     guests: 1,
@@ -9,6 +13,19 @@ const BookingForm = ({ availableTimes, submitForm }) => {
   };
 
   const [formData, setFormData] = useState(initialFormState);
+  const [formValid, setFormValid] = useState(false);
+
+  useEffect(() => {
+    // Check if form is valid whenever formData changes
+    setFormValid(
+      formData.name !== '' &&
+      formData.email.includes('@') &&
+      formData.phoneNumber.length >= 10 &&
+      formData.date !== '' &&
+      formData.guests >= 1 &&
+      formData.guests <= 10
+    );
+  }, [formData]);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -16,53 +33,68 @@ const BookingForm = ({ availableTimes, submitForm }) => {
       ...prevFormData,
       [name]: value,
     }));
-
-    if (name === 'date') {
-      // Dispatch an action to update availableTimes state when the date is changed
-      // Dispatching not needed anymore since we're using the submitForm function
-    }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     // Call the submitForm function and pass the form data
-    submitForm(formData);
+    if (formValid) {
+      submitForm(formData);
+    }
   };
 
   return (
-    <form style={{ display: 'grid', maxWidth: '200px', gap: '20px' }} onSubmit={handleSubmit}>
-      <label htmlFor="res-date">Choose date</label>
-      <input type="date" id="res-date" name="date" value={formData.date} onChange={handleChange} />
+    <form className="booking-form" onSubmit={handleSubmit} aria-labelledby="bookingForm">
+      <div className="booking-form-left">
+        <label htmlFor="full-name">Full Name</label>
+        <input type="text" id="full-name" name="name" value={formData.name} onChange={handleChange} required aria-required="true" className={formData.name === '' ? 'invalid' : ''} />
 
-      <label htmlFor="res-time">Choose time</label>
-      <select id="res-time" name="time" value={formData.time} onChange={handleChange}>
-        {availableTimes.map((time) => (
-          <option key={time} value={time}>
-            {time}
-          </option>
-        ))}
-      </select>
+        <label htmlFor="phone-number">Phone Number</label>
+        <input type="tel" id="phone-number" name="phoneNumber" value={formData.phoneNumber} onChange={handleChange} required aria-required="true" className={formData.phoneNumber.length < 10 ? 'invalid' : ''} />
 
-      <label htmlFor="guests">Number of guests</label>
-      <input
-        type="number"
-        placeholder="1"
-        min="1"
-        max="10"
-        id="guests"
-        name="guests"
-        value={formData.guests}
-        onChange={handleChange}
-      />
+        <label htmlFor="email">Email Address</label>
+        <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required aria-required="true" className={!formData.email.includes('@') ? 'invalid' : ''} />
+      </div>
 
-      <label htmlFor="occasion">Occasion</label>
-      <select id="occasion" name="occasion" value={formData.occasion} onChange={handleChange}>
-        <option value="Birthday">Birthday</option>
-        <option value="Anniversary">Anniversary</option>
-      </select>
+      <div className="booking-form-right">
+        <label htmlFor="res-date">Choose date</label>
+        <input type="date" id="res-date" name="date" value={formData.date} onChange={handleChange} required aria-required="true" className={formData.date === '' ? 'invalid' : ''} />
 
-      {/* Use a button instead of an input for form submission */}
-      <button type="submit">Make Your reservation</button>
+        <label htmlFor="res-time">Choose time</label>
+        <select id="res-time" name="time" value={formData.time} onChange={handleChange} aria-required="true">
+          {availableTimes.map((time) => (
+            <option key={time} value={time}>
+              {time}
+            </option>
+          ))}
+        </select>
+
+        <label htmlFor="guests">Number of guests</label>
+        <input
+          type="number"
+          placeholder="1"
+          min="1"
+          max="10"
+          id="guests"
+          name="guests"
+          value={formData.guests}
+          onChange={handleChange}
+          required
+          aria-required="true"
+          className={formData.guests < 1 || formData.guests > 10 ? 'invalid' : ''}
+        />
+
+        <label htmlFor="occasion">Occasion</label>
+        <select id="occasion" name="occasion" value={formData.occasion} onChange={handleChange} aria-required="true">
+          <option value="Birthday">Birthday</option>
+          <option value="Anniversary">Anniversary</option>
+        </select>
+      </div>
+      <div className="button-container">
+        {/* Use a button instead of an input for form submission */}
+        <button type="submit" disabled={!formValid} aria-disabled={!formValid}>Make Your reservation</button>
+      </div>
+
     </form>
   );
 };
